ba5cdcda13cb10d86d47041f876ef148f0f88a8d
Nick.Haer <nhaer123@gmail.com>
2020-02-18 23:43:38 -0600

    added cursors to buttons

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./jumbo.module.css"
-
-
+import styles from './jumbo.module.css';
 
 function Jumbotron(props) {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.mainTitle}>Search googlebooks for a Great Read</h1>
-            <h3 className={styles.secondaryTitle}>Made with React, so you can react with a good book!</h3>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.mainTitle}>Search GoogleBooks for a Great Read</h1>
+      <h3 className={styles.secondaryTitle}>
+        Made with React, so you can react with a good book!
+      </h3>
+    </div>
+  );
 }
 
 export default Jumbotron;
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,22 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./nav.module.css";
+import styles from './nav.module.css';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function NavBar(props) {
-    return (
-        <div  className = {styles.container}>
-            <span className={styles.phrase}>Reading is RadioReactive!</span>
-            <Link className = {styles.navItem} to="/">Search Books</Link>
-            <Link className = {styles.navItem}  to="/saved">Saved Book</Link>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <span className={styles.phrase}>Reading is RadioReactive!</span>
+      <Link className={styles.navItem} to='/'>
+        Search Books
+      </Link>
+      <Link className={styles.navItem} to='/saved'>
+        Saved Book
+      </Link>
+    </div>
+  );
 }
 export default NavBar;
